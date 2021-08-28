8be67d14dde99389083440d7a67846679897cbfa
Abu Azis <muhammadazis801@gmail.com>
2021-08-28 07:15:51 +0700

    initial commit

---
+++ app/src/main/java/com/abuzaio/viewmodel/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.abuzaio.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+    val hello: LiveData<String> = MutableLiveData<String>().also { it.value = "Halo" }
+}
---
+++ app/src/main/java/com/abuzaio/viewmodel/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.abuzaio.viewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProviders.of(this)[HomeViewModel::class.java]
+        viewModel.hello.observe(this, Observer { tv_home.text = it })
+    }
+}
